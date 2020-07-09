0b2e40e1f92f43a7a4142b5941222d0b16324bd0
Gustavo Dias <dias.gustavo23@gmail.com>
2020-07-09 10:09:54 -0300

    Finalizado

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Aula35AspNETCORE.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
